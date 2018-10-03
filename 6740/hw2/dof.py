b82c91f7d747de589c296db8f1982ef557cf8639
Chen Liang <cliang73@gatech.edu>
2018-10-03 18:42:39 -0400

    dof

--- 6740/hw2/dof.py
+++ 6740/hw2/dof.py
@@ -3,12 +3,37 @@ import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeRegressor
 
 mu, sigma = 0, 1 # mean and standard deviation
-y = np.random.normal(mu, sigma, 100)
-u = np.random.normal(mu, sigma, 100)
-E_y = np.mean(y)
-E_u = np.mean(u)
+num_samples = 100
+num_features = 10
+num_iter = 10
 
-r_1 = DecisionTreeRegressor(random_state=0, max_leaf_nodes=1)
+x = np.random.normal(mu, sigma, [num_samples, num_features])
+y = np.random.normal(mu, sigma, num_samples)
+y_hat_5, y_hat_10 = np.zeros([num_samples, num_iter]), np.zeros([num_samples, num_iter])
+E_y = np.mean(y)
+
+
 r_5 = DecisionTreeRegressor(random_state=0, max_leaf_nodes=5)
 r_10 = DecisionTreeRegressor(random_state=0, max_leaf_nodes=10)
-fit(X, y, sample_weight=None, check_input=True, X_idx_sorted=None)
+
+for i in range(num_iter):	
+	r_5.fit(x, y)
+	y_hat_5[:, i] = r_5.predict(x)
+	r_10.fit(x, y)
+	y_hat_10[:, i] = r_10.predict(x)
+
+y_hat_1 = np.ones([num_samples]) * np.mean(y)
+y_hat_5 = np.mean(y_hat_5, axis=1)
+y_hat_10 = np.mean(y_hat_10, axis=1)
+E_y_hat_1 = np.ones([num_samples]) * np.mean(y_hat_1)
+E_y_hat_5 = np.ones([num_samples]) * np.mean(y_hat_5)
+E_y_hat_10 = np.ones([num_samples]) * np.mean(y_hat_10)
+print(y_hat_1.shape)
+print(y_hat_5.shape)
+print(y_hat_10.shape)
+c = np.sum((y-E_y)*(y_hat_1- E_y_hat_1))/num_samples
+a = np.sum((y-E_y)*(y_hat_5- E_y_hat_5))/num_samples
+b = np.sum((y-E_y)*(y_hat_10- E_y_hat_10))/num_samples
+print(c)
+print(a)
+print(b)
