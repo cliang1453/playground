c0822b2ed86abbde3fc254de55db510519f8b69b
Chen Liang <lian455@usc.edu>
2018-10-03 20:42:44 -0400

    dof corrected

--- 6740/hw2/dof.py
+++ 6740/hw2/dof.py
@@ -2,38 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeRegressor
 
-mu, sigma = 0, 1 # mean and standard deviation
-num_samples = 100
-num_features = 10
-num_iter = 10
+mu, sigma = 0, 1 
+N = 100 
+B = 1000
+F = 10
 
-x = np.random.normal(mu, sigma, [num_samples, num_features])
-y = np.random.normal(mu, sigma, num_samples)
-y_hat_5, y_hat_10 = np.zeros([num_samples, num_iter]), np.zeros([num_samples, num_iter])
-E_y = np.mean(y)
+x = np.random.normal(mu, sigma, [N, F])
+y = np.random.normal(mu, sigma, N)
+y_hat, y_draw = np.zeros([B, N]), np.zeros([B, N])
+cov = np.zeros(N)
 
 
-r_5 = DecisionTreeRegressor(random_state=0, max_leaf_nodes=5)
-r_10 = DecisionTreeRegressor(random_state=0, max_leaf_nodes=10)
+for num_nodes in [1, 5, 10, N]:
+	if num_nodes != 1:
+		r = DecisionTreeRegressor(random_state=0, min_samples_leaf=1, max_leaf_nodes=num_nodes)
 
-for i in range(num_iter):	
-	r_5.fit(x, y)
-	y_hat_5[:, i] = r_5.predict(x)
-	r_10.fit(x, y)
-	y_hat_10[:, i] = r_10.predict(x)
+	for b in range(B):
+		idx_b = np.random.randint(low=0, high=N, size=N)
+		x_b, y_b = x[idx_b], y[idx_b]
+		
+		if num_nodes == 1:
+			y_hat[b, :] = np.mean(y_b)*np.ones(N)
+		else:
+			r.fit(x_b, y_b)
+			y_hat[b, :] = r.predict(x_b)
+		y_draw[b, :] = y_b
+		
+	Ey_hat = np.mean(y_hat, axis=0)
+	Ey_draw = np.mean(y_draw, axis=0)
 
-y_hat_1 = np.ones([num_samples]) * np.mean(y)
-y_hat_5 = np.mean(y_hat_5, axis=1)
-y_hat_10 = np.mean(y_hat_10, axis=1)
-E_y_hat_1 = np.ones([num_samples]) * np.mean(y_hat_1)
-E_y_hat_5 = np.ones([num_samples]) * np.mean(y_hat_5)
-E_y_hat_10 = np.ones([num_samples]) * np.mean(y_hat_10)
-print(y_hat_1.shape)
-print(y_hat_5.shape)
-print(y_hat_10.shape)
-c = np.sum((y-E_y)*(y_hat_1- E_y_hat_1))/num_samples
-a = np.sum((y-E_y)*(y_hat_5- E_y_hat_5))/num_samples
-b = np.sum((y-E_y)*(y_hat_10- E_y_hat_10))/num_samples
-print(c)
-print(a)
-print(b)
+	for i in range(N):
+		cov[i] = np.mean((y_hat[:, i] - Ey_hat[i])*(y_draw[:, i]-Ey_draw[i]))
+
+	df = np.sum(cov)
+	print(df)
