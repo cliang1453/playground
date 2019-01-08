f89870f2b4f30b9f7c895d46db1eb911b749862b
Chen Liang <lian455@usc.edu>
2019-01-08 17:13:29 -0500

    delete2

--- 6740/hw3_code_data_fies_revised/dirichlet_logprob.py
+++
@@ -1,9 +0,0 @@
-from scipy.special import gammaln
-import math
-import numpy as np 
-def dirichlet_logprob(alpha, x):
-    """ Log probability for Dirichlet distribution """
-    # alpha = K-dim. vector of Dirichlet parameters
-    logProb = gammaln(np.sum(alpha)) - np.sum(gammaln(alpha)) 
-    logProb = logProb + np.sum(np.log(x) * (alpha-1))
-    return logProb
