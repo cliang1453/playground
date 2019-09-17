60ac97ec0f85c6177f9bd6871341ad64a28c5af8
Chen Liang <lian455@usc.edu>
2019-09-17 03:30:39 -0400

    update

--- NumericalLinearAlgebra/hw1/gaussian_elimination.py
+++ NumericalLinearAlgebra/hw1/gaussian_elimination.py
@@ -1,41 +1,65 @@
 import numpy as np
-from utils import plot_solutions, plot_runtime, plot_comparison
+from utils import plot_solutions, plot_sol_comparison, plot_runtime_comparison
 import time
 
 class GaussianElimination(object):
-	"""docstring for GaussianElimination"""
+	
+	"""
+	Implementation of a solver for non-homogeneous one-dimensional linear ODE in the form of 
+	au^{''} + bu = cx^{2} + d, x \in [xb1, xb2], u(0) and u(1) bounary condition
+	using centered difference scheme with specified number of discretization
+	"""
 	def __init__(self, num_discretization = None):
-		super(GaussianElimination, self).__init__():
+		super(GaussianElimination, self).__init__()
 		self.num_discretization = num_discretization
 
 	def descritize(self, n = 200, lam = 0, params = None):
 
-		h = (params["xb1"] - params["xb2"]) / (n + 1)
+		h = (params["xb2"] - params["xb1"]) / (n + 1)
 		A = np.eye(n, k = 0) * (-2 * params["a"] / (h ** 2) + lam) \
 		    + np.eye(n, k = 1) * params["a"] / (h ** 2) \
 		    + np.eye(n, k = -1) * params["a"] / (h ** 2)
 
-		b = params["c"] * np.array([for x**2 in x in range(xb1, xb2, h)]) + params["d"]
+		b = params["c"] * np.arange(params["xb1"] + h, params["xb2"], h) ** 2 + params["d"]
+		b[0] += params["u(0)"] / h**2
+		b[-1] +=  params["u(1)"] / h**2
 		return A, b
 
 	def naive(self, A = None):
 		
 		m = A.shape[0]
-		L, U = np.ones(m), np.copy(A)
+		L, U = np.eye(m), np.copy(A)
 
 		for k in range(m - 1): 
-			
-			L[k + 1:][k] = U[k + 1:][k] / U[k][k]
+			L[k + 1:,k] = U[k + 1:,k] / U[k,k]
 			for j in range(k + 1, m): 
-				U[j][k:] -= L[j][k] * U[k][k:]
+				U[j,k:] -= L[j,k] * U[k,k:]
 
 		return L, U
+
+	def pivoting(self, A = None):
+		
+		m = A.shape[0]
+		L, U, P = np.eye(m), np.copy(A), np.eye(m)
+
+		for k in range(m-1):
+			
+			i = np.argmax(U[k:,k]) + k
+			U[k,k:], U[i,k:] = U[i,k:], U[k,k:].copy()
+			L[k,:k], L[i,:k] = L[i,:k], L[k,:k].copy()
+			P[[k, i]] = P[[i, k]]
+
+			L[k + 1:,k] = U[k + 1:,k] / U[k,k]
+			for j in range(k + 1, m):
+				U[j,k:] -= L[j,k] * U[k,k:]
+
+		return L, U, P
 
 	def forward(self, L = None, b = None):
 
 		y = np.copy(b)
 		for k in range(y.shape[0]):
-			y[k] -= np.sum(L[:k][k] * y[:k])
+			y[k] -= np.sum(L[k,:k] * y[:k])
 
 		return y
 
@@ -43,27 +67,10 @@ class GaussianElimination(object):
 
 		x = np.copy(y)
 		for k in range(x.shape[0] - 1, -1, -1):
-			x[k] -= np.sum(U[k + 1:][k] * x[k + 1:])
+			x[k] -= np.sum(U[k,k + 1:] * x[k + 1:])
+			x[k] = x[k] / U[k][k]
 
 		return x
-
-	def pivoting(self, A = None):
-		
-		m = A.shape[0]
-		L, U, P = np.ones(m), np.copy(A), np.ones(m)
-
-		for k in range(m-1):
-			
-			i = np.argmax(U[k:][k]) + k
-			U[k][k:], U[i][k:] = U[i][k:], U[k][k:].copy()
-			L[k][:k], L[i][:k] = L[i][:k], L[k][:k].copy()
-			P[[k, i]] = P[[i, k]]
-
-			L[k + 1:][k] = U[k + 1:][k] / U[k][k]
-			for j in range(k + 1, m):
-				U[j][k:] -= L[j][k] * U[k][k:]
-
-		return L, U, P
 
 	def run(self, params = None, mode = "Pivoting"):
 
@@ -78,15 +85,17 @@ class GaussianElimination(object):
 			for n in self.num_discretization:
 			
 				A, b = self.descritize(n, lam, params)
-				start = time.perf_counter()
+				start = time.time()
 				
 				if mode == "Naive":
 					L, U = self.naive(A)
 				elif mode == "Pivoting":
 					L, U, P = self.pivoting(A)
-					A = np.matmul(P, A)
 					b = np.matmul(P, b)
+				else:
+					ValueError("Mode not supported.")
 
+				
 				# forward substitution: Ly = b -> y
 				y = self.forward(L = L, b = b)
 
@@ -99,20 +108,14 @@ class GaussianElimination(object):
 		return xs, times
 				
 
-def main():
-
-	# params 
-	num_discretization = [200, 400, 800, 1000, 2000, 4000]
-	# params for solving the non-homogeneous one-dimensional ODE in form of 
-	# au^{''} + bu = cx^{2} + d, x \in [xb1, xb2]
-	# using centered difference scheme
-	params = {"a": -1, "b": [0, 8], "c": 3, "d": -0.5, "xb1": 0.0, "xb2": 1.0}
+def main(num_discretization = None, params = None, exact_solution = None):
 
 	# solver
 	ge_solver = GaussianElimination(num_discretization = num_discretization)
 	solution_naive, runtime_naive = ge_solver.run(params = params, mode = "Naive")
 	solution_pivoting, runtime_pivoting = ge_solver.run(params = params, mode = "Pivoting")
 
+	# plot
 	plot_solutions(params, num_discretization, solution_naive, exact_solution, "Naive")
 	plot_solutions(params, num_discretization, solution_pivoting, exact_solution, "Pivoting")
 	
@@ -121,5 +124,12 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
+
+	# params 
+	num_discretization = [200, 400, 800, 1000, 2000, 4000]
+	params = {"a": -1, "b": [0, 8], "c": 3, "d": -0.5, "xb1": 0, "xb2": 1, "u(0)": 1, "u(1)": -1}
+	exact_solution = {0: [-0.25, 0.25, -2, 1], 
+					  8: [0.375, 1.05556, -2.82843, -0.086805, 2.82843, 0.03125]}
+
+	main(num_discretization, params, exact_solution)
 		
--- NumericalLinearAlgebra/hw1/utils.py
+++ NumericalLinearAlgebra/hw1/utils.py
@@ -1,6 +1,8 @@
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import numpy as np
+from textwrap import wrap
 
 def plot_labels(x_label = "", y_label = "", title = "", name = ""):
 
@@ -9,7 +11,7 @@ def plot_labels(x_label = "", y_label = "", title = "", name = ""):
 
 	plt.xlabel(x_label)
 	plt.ylabel(y_label)
-	plt.title(title)
+	plt.title("\n".join(wrap(title, 60)), fontsize=10)
 	plt.legend()
 	plt.savefig(name + ".png")
 	plt.close()
@@ -24,24 +26,39 @@ def plot_norm(x = None, y = None, x_label = "", y_label = "", title = "", name =
 
 def plot_solutions(params = None, num_discretization = None, solution = None, exact_solution = None, mode = "Pivoting"):
 
+	h_exact = (params["xb2"] - params["xb1"]) / (4000 + 1)
+	x_exact = np.arange(params["xb1"] + h_exact, params["xb2"], h_exact)
+
 	for lam in params["b"]:
 
-		for i in len(num_discretization):
-			n = num_discretization[i]
-			x = np.array(params["xb1"], params["xb2"], (params["xb2"] - params["xb1"]) / (n + 1))
-			y = solution[lam][i]
-			plt.plot(x, y, label= "# interior mesh points =" + str(n))
+		if lam == 0:
+			c1, c2, c3, c4 = exact_solution[lam]
+			y_exact = c1 * x_exact ** 4 + c2 * x_exact ** 2 + c3 * x_exact + c4
+		else:
+			c1, c2, c3, c4, c5, c6 = exact_solution[lam]
+			y_exact = c1 * x_exact ** 2 + c2 * np.exp(c3 * x_exact) + c4 * np.exp(c5 * x_exact) + c6
 
-		name = mode + " GE solution using centered difference scheme vs Exact solution for 1D nonhomogeneous ODE at lambda = " + str(lam)
-		plot_labels("x", "u(x)", name, name.split(' ').join('_'))		
+		plt.plot(x_exact, y_exact, label= "exact_solution")
+
+		for i in range(len(num_discretization)):
+			n = num_discretization[i]
+			h = (params["xb2"] - params["xb1"]) / (n + 1)
+			x = np.arange(params["xb1"] + h, params["xb2"], h)
+			y = solution[lam][i]
+			plt.plot(x, y, label= "# interior mesh points=" + str(n))
+			
+
+		name = mode + " GE solution using centered difference scheme vs Exact solution for 1D nonhomogeneous ODE at lambda=" + str(lam)
+		plot_labels("x", "u(x)", name, "_".join(name.split(' ')))		
 
 
 def plot_sol_comparison(params = None, num_discretization = None, solution_naive = None, solution_pivoting = None):
 
-	for i in num_discretization:
+	for i in range(len(num_discretization)):
 		
 		n = num_discretization[i]
-		x = np.array(params["xb1"], params["xb2"], (params["xb2"] - params["xb1"]) / (n + 1))
+		h = (params["xb2"] - params["xb1"]) / (n + 1)
+		x = np.arange(params["xb1"] + h, params["xb2"], h)
 
 		for lam in params["b"]:
 			
@@ -51,8 +68,8 @@ def plot_sol_comparison(params = None, num_discretization = None, solution_naive
 			plt.plot(x, y_naive, label= "naive GE solution, lambda =" + str(lam))
 			plt.plot(x, y_pivoting, label= "pivoting GE solution, lambda =" + str(lam))
 
-		name = "Comparison of naive GE solution vs pivoting GE solution using centered difference scheme for 1D nonhomogeneous ODE with # interior mesh points = " + str(n)
-		plot_labels("x", "u(x)", name, name.split(' ').join('_'))
+		name = "Comparison of naive GE solution vs pivoting GE solution using centered difference scheme for 1D nonhomogeneous ODE with # interior mesh points=" + str(n)
+		plot_labels("x", "u(x)", name, "_".join(name.split(' ')))
 
 
 def plot_runtime_comparison(params = None, num_discretization = None, runtime_naive = None, runtime_pivoting = None):
@@ -63,7 +80,7 @@ def plot_runtime_comparison(params = None, num_discretization = None, runtime_na
 		y_pivoting = runtime_pivoting[lam]
 
 		plt.plot(num_discretization, y_naive, label= "naive GE solution, lambda =" + str(lam))
-		plt.plot(x, y_pivoting, label= "pivoting GE solution, lambda =" + str(lam))
+		plt.plot(num_discretization, y_pivoting, label= "pivoting GE solution, lambda =" + str(lam))
 		
-	name = "Runtime comparison of naive GE vs pivoting GE solution using centered difference scheme for 1D nonhomogeneous ODE at lambda = " + str(lam)
-	plot_labels("# interior mesh points", "runtime (ms)", name, name.split(' ').join('_'))
+		name = "Runtime comparison of naive GE vs pivoting GE solution using centered difference scheme for 1D nonhomogeneous ODE at lambda=" + str(lam)
+		plot_labels("# interior mesh points", "runtime (ms)", name, "_".join(name.split(' ')))
