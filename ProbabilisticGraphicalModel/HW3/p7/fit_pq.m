04c91280d08e5c88788fbaa7fc0bc55a647e5c73
Chen Liang <lian455@usc.edu>
2020-04-04 15:05:14 -0400

    p7

---
+++ ProbabilisticGraphicalModel/HW3/p7/fit_pq.m
@@ -0,0 +1,20 @@
+import brml.*
+load p
+p=setpotclass(p,'array');
+x = p.variables(1);
+y = p.variables(2);
+z = p.variables(3);
+size = p.size;
+q_z = rand([size(3) 1]);
+q_z = array(z, q_z./sum(sum(q_z)));
+
+T = 100;
+for t=1:T
+    q_xy = condpot(exppot(sumpot(multpots([logpot(p) q_z]),z)));
+    q_z = condpot(exppot(sumpot(multpots([logpot(p) q_xy]),[x y])));
+    q_xyz = multpots([q_xy q_z]);
+    KL(t) = KLdiv(q_xyz, p);
+end;
+fprintf(1,'Minimum KL-divergence is = %g\n',KL(T));
+approx = table(q_xyz);
+fprintf(1,'Mean deviation between p and q approx = %g\n',mean(abs(approx(:)-p.table(:))))
